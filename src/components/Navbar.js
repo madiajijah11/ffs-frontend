43edff5cdac60f2df4498e50c1cbd3579432942c
MexL <madiajijah7@gmail.com>
2022-12-16 19:56:20 +0800

    menambahkan padding pada navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const themes = [
   "light",
   "dark",
@@ -33,7 +35,7 @@ const themes = [
 const Navbar = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 px-20 py-6">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -52,77 +54,11 @@ const Navbar = () => {
                 />
               </svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a href="#" className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a href="#">Submenu 1</a>
-                  </li>
-                  <li>
-                    <a href="#">Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Item 3</a>
-              </li>
-            </ul>
           </div>
-          <a href="#" className="btn btn-ghost normal-case text-xl">
+          <Link to="#" className="btn btn-ghost normal-case text-xl">
             FFS
-          </a>
+          </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a href="#">Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a href="#">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a href="#">Submenu 1</a>
-                </li>
-                <li>
-                  <a href="#">Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Item 3</a>
-            </li>
-          </ul>
-        </div>
-
         <div className="navbar-end">
           <div className="dropdown dropdown-end mr-1">
             <div tabIndex={0} className="btn gap-1 normal-case btn-ghost">
@@ -174,9 +110,12 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <a href="#" className="btn">
-            Get started
-          </a>
+          <Link to="#" className="btn btn-outline btn-primary mr-2">
+            Masuk
+          </Link>
+          <Link to="#" className="btn btn-primary">
+            Daftar
+          </Link>
         </div>
       </div>
     </>
