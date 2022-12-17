1560ac7e20b89fb668adb098ac720189ee731afb
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-17 16:19:50 +0700

    add register recruiter page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { themeChange } from "theme-change";
 import LoginRecruiter from "./pages/LoginRecruiter";
 import Sample from "./pages/sample";
+import RegisterRecruiter from "./pages/ResgisterRecruiter";
 
 function App() {
   useEffect(() => {
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route index path="/" element={<Sample />} />
       <Route path="/login-recruiter" element={<LoginRecruiter />} />
+      <Route path="/register-recruiter" element={<RegisterRecruiter />} />
     </Routes>
   );
 }
