245c31ec7dd5650054f5f7feb35ca47f9891b5e2
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-17 20:59:14 +0700

    edit styling chatblank page

--- src/pages/ChatBlank.js
+++ src/pages/ChatBlank.js
@@ -8,7 +8,7 @@ const ChatBlank = () => {
       <Navbar />
       <div className="px-2 sm:px-20 py-6 bg-[#E5E5E5]">
         <div className="flex max-[768px]:flex-col">
-          <div className="w-1/4 bg-white rounded-md mr-10 h-[75vh] max-[768px]:w-full max-[768px]:mb-6">
+          <div className="w-1/3 bg-white rounded-md mr-10 h-[75vh] max-[768px]:w-full max-[768px]:mb-6">
             <h3 className="font-semibold leading-6 bold text-base	p-5">Chat</h3>
             <div className="border-1 border border-[#E2E5ED]"></div>
             <div className="h-full flex flex-col items-center justify-center">
@@ -16,7 +16,7 @@ const ChatBlank = () => {
               <h3 className="text-center font-semibold	text-base	leading-6	">Belum ada chat</h3>
             </div>
           </div>
-          <div className="w-3/4 bg-white rounded-md max-[768px]:w-full">
+          <div className="w-2/3 bg-white rounded-md max-[768px]:w-full">
             <h3 className="font-semibold leading-6 bold text-base	p-5 text-white">cobacoba</h3>
             <div className="border-1 border border-[#E2E5ED]"></div>
           </div>
