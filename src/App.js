f0ce59ff175d414113073c523f1a4202a69d3f16
Faisal Chakiki <faisalchakiki012018@gmail.com>
2022-12-17 22:20:33 +0700

    add profile reqruiter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { themeChange } from "theme-change";
 import Sample from "./pages/Sample";
 import LoginEmployee from "./pages/LoginEmployee";
 import LoginRecruiter from "./pages/LoginRecruiter";
-import RegisterRecruiter from "./pages/ResgisterRecruiter";
+import RegisterRecruiter from "./pages/RegisterRecruiter";
 import ResetPassword from "./pages/ResetPassword";
 import Register from "./pages/Register";
 import ConfirmPassword from "./pages/ConfirmPassword";
 import JobExperience from "./pages/JobExperience";
 import HirePage from "./pages/HirePage"
 import Home from "./pages/Home";
+import ProfileReqruiter from "./pages/ProfileReqruiter";
 
 function App() {
   useEffect(() => {
@@ -21,15 +22,16 @@ function App() {
   return (
     <Routes>
       <Route index path="/" element={<Sample />} />
-      <Route path="/loginemployee" element={<LoginEmployee />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login-employee" element={<LoginEmployee />} />
       <Route path="/login-recruiter" element={<LoginRecruiter />} />
       <Route path="/register-recruiter" element={<RegisterRecruiter />} />
       <Route path="/register" element={<Register />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/confirm-password" element={<ConfirmPassword />} />
       <Route path="/job-experience" element={<JobExperience />} />
-      <Route path="/HirePage" element={<HirePage />} />
-      <Route path="/Home" element={<Home />} />
+      <Route path="/hirePage" element={<HirePage />} />
+      <Route path="/profil-reqruiter" element={<ProfileReqruiter />} />
     </Routes>
   );
 }
