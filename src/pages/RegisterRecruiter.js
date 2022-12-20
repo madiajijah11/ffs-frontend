a6f2ca4c3afacdfaad83d6ec572e0542a53bb6da
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-20 16:14:22 +0700

    integrasi ke endpoint register recruiter

--- src/pages/RegisterRecruiter.js
+++ src/pages/RegisterRecruiter.js
@@ -1,4 +1,54 @@
+import axios from 'axios'
+import React from 'react'
+
 const RegisterRecruiter = () => {
+  const [showAlertName, setShowAlertName] = React.useState(false)
+  const [showAlertEmail, setShowAlertEmail] = React.useState(false)
+  const [showAlertCompanyName, setShowAlertCompanyName] = React.useState(false)
+  const [showAlertCompanyField, setShowAlertCompanyField] = React.useState(false)
+  const [showAlertPhoneNumber, setShowAlertPhoneNumber] = React.useState(false)
+  const [showAlertPassword, setShowAlertPassword] = React.useState(false)
+  const [showAlertConfirmPassword, setShowAlertConfirmPassword] = React.useState(false)
+
+  const register = async (event) => {
+    event.preventDefault()
+    const name = event.target.name.value
+    const email = event.target.email.value
+    const companyName = event.target.companyName.value
+    const companyField = event.target.companyField.value
+    const phoneNumber = event.target.phoneNumber.value
+    const password = event.target.password.value
+    const confirmPassword = event.target.confirmPassword.value
+
+    if (!name) {
+      setShowAlertName(true)
+    } else if (!email) {
+      setShowAlertEmail(true)
+    } else if (!companyName) {
+      setShowAlertCompanyName(true)
+    } else if (!companyField) {
+      setShowAlertCompanyField(true)
+    } else if (!phoneNumber) {
+      setShowAlertPhoneNumber(true)
+    } else if (!password) {
+      setShowAlertPassword(true)
+    } else if ( confirmPassword !== password) {
+      setShowAlertConfirmPassword(true)
+    } else {
+      const {data} = await axios.post('http://localhost:8888/users', {
+        fullName: name,
+        email,
+        phoneNumber,
+        password,
+        companyName,
+        companyField,
+        groupUser: 2
+      })
+      return data
+    }
+  }
+
+
   return(
     <div className="flex font-sans h-screen">
       <div className="flex-1 relative max-[600px]:hidden overflow-hidden">
@@ -14,34 +64,41 @@ const RegisterRecruiter = () => {
         <img className='w-32 top-[30px] left-[30px] mb-10 min-[600px]:hidden' src={require('../assets/images/logoUngu.png')} alt='Logo'/>
         <h1 className='font-bold text-2xl mb-2'>Halo, Pewpeople</h1>
         <p className='mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
-        <form>
+        <form onSubmit={register}>
           <div className='mb-5'>
             <label className='block mb-2'>Nama</label>
-            <input name='email' type='text' placeholder='Masukan nama panjang' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertName(false)} name='name' type='text' placeholder='Masukan nama panjang' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertName ? <p className='text-sm text-red-700'>Please fill in the name</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>Email</label>
-            <input name='email' type='text' placeholder='Masukan alamat email' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertEmail(false)} name='email' type='text' placeholder='Masukan alamat email' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertEmail ? <p className='text-sm text-red-700'>Please fill in the email</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>Perusahaan</label>
-            <input name='email' type='text' placeholder='Masukan nama perusahaan' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertCompanyName(false)} name='companyName' type='text' placeholder='Masukan nama perusahaan' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertCompanyName ? <p className='text-sm text-red-700'>Please fill in the company name</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>Bidang Perusahaan</label>
-            <input name='email' type='text' placeholder='Bidang perusahaan Anda' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertCompanyField(false)} name='companyField' type='text' placeholder='Bidang perusahaan Anda' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertCompanyField ? <p className='text-sm text-red-700'>Please fill in the company field</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>No Handphone</label>
-            <input name='email' type='text' placeholder='Masukan no handphone' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertPhoneNumber(false)} name='phoneNumber' type='text' placeholder='Masukan no handphone' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertPhoneNumber ? <p className='text-sm text-red-700'>Please fill in the phone number</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>Kata Sandi</label>
-            <input name='email' type='text' placeholder='Masukan kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertPassword(false)} name='password' type='password' placeholder='Masukan kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertPassword ? <p className='text-sm text-red-700'>Please fill in the password</p> : false}
           </div>
           <div className='mb-5'>
             <label className='block mb-2'>Konfirmasi kata Sandi</label>
-            <input name='email' type='text' placeholder='Masukan konfirmasi kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            <input onChange={() => setShowAlertConfirmPassword(false)} name='confirmPassword' type='password' placeholder='Masukan konfirmasi kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
+            {showAlertConfirmPassword ? <p className='text-sm text-red-700'>Confirm password does not match password</p> : false}
           </div>
           <div className='mb-5'>
             <button className='border-[1px] border-solid border-[#FBB017] bg-warning w-[100%] pl-3 h-[50px] rounded-[4px] text-white'>Daftar</button>
