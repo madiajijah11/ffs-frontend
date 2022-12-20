86ef85b1e36c420354cb832c7ebd57447b147ef2
MexL <madiajijah7@gmail.com>
2022-12-21 01:27:29 +0800

    crete profile page berdasarkan role

--- src/pages/LoginEmployee.js
+++ src/pages/LoginEmployee.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { LoginEmployee as loginAction } from "../redux/actions/authAction";
 
 const LoginEmployee = () => {
