2144f13f85bcc036fca362da3890b217b5c61378
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-20 12:37:58 +0700

    creating responsive for job experience page

--- src/pages/JobExperience.js
+++ src/pages/JobExperience.js
@@ -6,10 +6,10 @@ const JobExperience = () => {
   return (
     <div>
       <NavHome></NavHome>
-      <section className="bg-primary h-[40vh]"></section>
+      <section className="md:bg-primary h-[40vh]"></section>
       <section className="bg-[#EAEAEA] px-[100px] pb-[20vh]">
-        <div className="flex gap-x-3">
-          <div className="bg-white mt-[-20vh] w-[30%] border rounded-md">
+        <div className="md:flex md:gap-x-3">
+          <div className="bg-white mt-[-20vh] w-[30%] border rounded-md sm:w-full">
             <div className="flex items-center justify-center">
               <img src={require('../assets/images/profile.png')} className="mt-[30px]" alt='Profile' />
             </div>
