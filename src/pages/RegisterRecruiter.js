a51d5caacec24cc749f3489055f45d540f4628a9
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-20 21:42:15 +0700

    add validator register recruiter

--- src/pages/RegisterRecruiter.js
+++ src/pages/RegisterRecruiter.js
@@ -1,54 +1,41 @@
-import axios from 'axios'
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { registerRecruiter } from "../redux/actions/authAction";
+import { Form, Field, Formik } from "formik";
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+import { Icon } from "@iconify/react";
+
+YupPassword(Yup);
+
+const RegisterRecruiterSchema = Yup.object().shape({
+  fullName: Yup.string().required("Nama harus diisi"),
+  email: Yup.string().email("Email tidak valid").required("Email harus diisi"),
+  phoneNumber: Yup.string()
+    .required("Nomor telepon harus diisi")
+    .matches(/^[0-9]+$/, "Nomor telepon harus angka")
+    .min(10, "Nomor telepon minimal 10 karakter")
+    .min(13, "Nomor telepon maksimal 13 karakter"),
+  password: Yup.string()
+    .required("Password harus diisi")
+    .min(6, "Password minimal 6 karakter")
+    .minLowercase(1, "Password minimal 1 huruf kecil")
+    .minUppercase(1, "Password minimal 1 huruf besar")
+    .minNumbers(1, "Password minimal 1 angka")
+    .minSymbols(1, "Password minimal 1 simbol"),
+  confirmPassword: Yup.string()
+    .required("Konfirmasi password harus diisi")
+    .oneOf([Yup.ref("password"), null], "Password tidak sama"),
+  companyName: Yup.string().required("Nama perusahaan harus diisi"),
+  companyField: Yup.string().required("Bidang perusahaan harus diisi"),
+});
 
 const RegisterRecruiter = () => {
-  const [showAlertName, setShowAlertName] = React.useState(false)
-  const [showAlertEmail, setShowAlertEmail] = React.useState(false)
-  const [showAlertCompanyName, setShowAlertCompanyName] = React.useState(false)
-  const [showAlertCompanyField, setShowAlertCompanyField] = React.useState(false)
-  const [showAlertPhoneNumber, setShowAlertPhoneNumber] = React.useState(false)
-  const [showAlertPassword, setShowAlertPassword] = React.useState(false)
-  const [showAlertConfirmPassword, setShowAlertConfirmPassword] = React.useState(false)
-
-  const register = async (event) => {
-    event.preventDefault()
-    const name = event.target.name.value
-    const email = event.target.email.value
-    const companyName = event.target.companyName.value
-    const companyField = event.target.companyField.value
-    const phoneNumber = event.target.phoneNumber.value
-    const password = event.target.password.value
-    const confirmPassword = event.target.confirmPassword.value
-
-    if (!name) {
-      setShowAlertName(true)
-    } else if (!email) {
-      setShowAlertEmail(true)
-    } else if (!companyName) {
-      setShowAlertCompanyName(true)
-    } else if (!companyField) {
-      setShowAlertCompanyField(true)
-    } else if (!phoneNumber) {
-      setShowAlertPhoneNumber(true)
-    } else if (!password) {
-      setShowAlertPassword(true)
-    } else if ( confirmPassword !== password) {
-      setShowAlertConfirmPassword(true)
-    } else {
-      const {data} = await axios.post('http://localhost:8888/users', {
-        fullName: name,
-        email,
-        phoneNumber,
-        password,
-        companyName,
-        companyField,
-        groupUser: 2
-      })
-      return data
-    }
-  }
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { error, loading } = useSelector((state) => state.auth);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="flex font-sans h-screen">
@@ -72,69 +59,198 @@ const RegisterRecruiter = () => {
       </div>
 
       <div className="flex-1 flex flex-col py-10 px-14 bg-[#E5E5E5] max-[600px]:p-5 overflow-y-scroll">
-        <img className='w-32 top-[30px] left-[30px] mb-10 min-[600px]:hidden' src={require('../assets/images/logoUngu.png')} alt='Logo'/>
-        <h1 className='font-bold text-2xl mb-2'>Halo, Pewpeople</h1>
-        <p className='mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
-        <form onSubmit={register}>
-          <div className='mb-5'>
-            <label className='block mb-2'>Nama</label>
-            <input onChange={() => setShowAlertName(false)} name='name' type='text' placeholder='Masukan nama panjang' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertName ? <p className='text-sm text-red-700'>Please fill in the name</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>Email</label>
-            <input onChange={() => setShowAlertEmail(false)} name='email' type='text' placeholder='Masukan alamat email' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertEmail ? <p className='text-sm text-red-700'>Please fill in the email</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>Perusahaan</label>
-            <input onChange={() => setShowAlertCompanyName(false)} name='companyName' type='text' placeholder='Masukan nama perusahaan' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertCompanyName ? <p className='text-sm text-red-700'>Please fill in the company name</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>Bidang Perusahaan</label>
-            <input onChange={() => setShowAlertCompanyField(false)} name='companyField' type='text' placeholder='Bidang perusahaan Anda' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertCompanyField ? <p className='text-sm text-red-700'>Please fill in the company field</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>No Handphone</label>
-            <input onChange={() => setShowAlertPhoneNumber(false)} name='phoneNumber' type='text' placeholder='Masukan no handphone' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertPhoneNumber ? <p className='text-sm text-red-700'>Please fill in the phone number</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>Kata Sandi</label>
-            <input onChange={() => setShowAlertPassword(false)} name='password' type='password' placeholder='Masukan kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertPassword ? <p className='text-sm text-red-700'>Please fill in the password</p> : false}
-          </div>
-          <div className='mb-5'>
-            <label className='block mb-2'>Konfirmasi kata Sandi</label>
-            <input onChange={() => setShowAlertConfirmPassword(false)} name='confirmPassword' type='password' placeholder='Masukan konfirmasi kata sandi' className='border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]'></input>
-            {showAlertConfirmPassword ? <p className='text-sm text-red-700'>Confirm password does not match password</p> : false}
-          </div>
-          <div className="mb-5">
-            <button className="border-[1px] border-solid border-[#FBB017] bg-warning w-[100%] pl-3 h-[50px] rounded-[4px] text-white">
-              Daftar
-            </button>
-          </div>
-          <p className="text-center">
-            Anda sudah punya akun?{" "}
-            <Link
-              to="/login-recruiter"
-              className="text-warning hover:font-bold"
-            >
-              Masuk disini
-            </Link>
-          </p>
-          <p className="text-center">
-            Login sebagai Pekerja?{" "}
-            <Link
-              to="/login-employee"
-              className="text-warning hover:font-bold"
-            >
-              Masuk disini
-            </Link>
-          </p>
-        </form>
+        <img
+          className="w-32 top-[30px] left-[30px] mb-10 min-[600px]:hidden"
+          src={require("../assets/images/logoUngu.png")}
+          alt="Logo"
+        />
+        <h1 className="font-bold text-2xl mb-2">Halo, Pewpeople</h1>
+        <p className="mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+          ipsum et dui rhoncus auctor.
+        </p>
+        <Formik
+          initialValues={{
+            fullName: "",
+            email: "",
+            phoneNumber: "",
+            password: "",
+            confirmPassword: "",
+            companyName: "",
+            companyField: "",
+          }}
+          validationSchema={RegisterRecruiterSchema}
+          onSubmit={(values) => {
+            // dispatch(
+            //   registerRecruiter({...values, cb:() => navigate("/"),})
+            // )
+            console.log(values);
+          }}
+        >
+          {({ errors, touched, dirty }) => {
+            <Form>
+              <div className="mb-5">
+                <label className="block mb-2" htmlFor="fullName">
+                  Nama
+                </label>
+                <Field
+                  name="fullName"
+                  type="text"
+                  placeholder="Masukan nama panjang"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {errors.fullName && touched.fullName ? (
+                  <div className="text-red-500 text-sm">{errors.fullName}</div>
+                ) : null}
+              </div>
+              <div className="mb-5">
+                <label className="block mb-2" htmlFor="email">
+                  Email
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="Masukan alamat email"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {errors.email && touched.email ? (
+                  <div className="text-red-500 text-sm">{errors.email}</div>
+                ) : null}
+              </div>
+              <div className="mb-5">
+                <label className="block mb-2" htmlFor="companyName">
+                  Perusahaan
+                </label>
+                <Field
+                  name="companyName"
+                  type="text"
+                  placeholder="Masukan nama perusahaan"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {errors.companyName && touched.companyName ? (
+                  <div className="text-red-500 text-sm">
+                    {errors.companyName}
+                  </div>
+                ) : null}
+              </div>
+              <div className="mb-5">
+                <label className="block mb-2" htmlFor="companyField">
+                  Bidang Perusahaan
+                </label>
+                <Field
+                  name="companyField"
+                  type="text"
+                  placeholder="Bidang perusahaan Anda"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {errors.companyField && touched.companyField ? (
+                  <div className="text-red-500 text-sm">
+                    {errors.companyField}
+                  </div>
+                ) : null}
+              </div>
+              <div className="mb-5">
+                <label className="block mb-2" htmlFor="phoneNumber">
+                  No Handphone
+                </label>
+                <Field
+                  name="phoneNumber"
+                  type="text"
+                  placeholder="Masukan no handphone"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {errors.phoneNumber && touched.phoneNumber ? (
+                  <div className="text-red-500 text-sm">
+                    {errors.phoneNumber}
+                  </div>
+                ) : null}
+              </div>
+              <div className="mb-5 relative">
+                <label className="block mb-2" htmlFor="password">
+                  Kata Sandi
+                </label>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Masukan kata sandi"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {showPassword ? (
+                  <Icon
+                    className="absolute top-9 right-4 w-10 h-10"
+                    icon="mdi:eye"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                ) : (
+                  <Icon
+                    className="absolute top-9 right-4 w-10 h-10"
+                    icon="mdi:eye-off"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                )}
+                {errors.password && touched.password ? (
+                  <div className="text-red-500 text-sm">{errors.password}</div>
+                ) : null}
+              </div>
+              <div className="mb-5 relative">
+                <label className="block mb-2" htmlFor="confirmPassword">
+                  Konfirmasi kata Sandi
+                </label>
+                <Field
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Masukan konfirmasi kata sandi"
+                  className="border-[1px] border-solid border-neutral bg-white w-[100%] pl-3 h-[50px] rounded-[4px]"
+                />
+                {showPassword ? (
+                  <Icon
+                    className="absolute top-9 right-4 w-10 h-10"
+                    icon="mdi:eye"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                ) : (
+                  <Icon
+                    className="absolute top-9 right-4 w-10 h-10"
+                    icon="mdi:eye-off"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                )}
+                {errors.confirmPassword && touched.confirmPassword ? (
+                  <div className="text-red-500 text-sm">
+                    {errors.confirmPassword}
+                  </div>
+                ) : null}
+              </div>
+              <div className="mb-5">
+                <button
+                  className="border-[1px] border-solid border-[#FBB017] bg-warning w-[100%] pl-3 h-[50px] rounded-[4px] text-white"
+                  type="submit"
+                  disabled={!dirty || loading}
+                >
+                  Daftar
+                </button>
+              </div>
+              <p className="text-center">
+                Anda sudah punya akun?{" "}
+                <Link
+                  to="/login-recruiter"
+                  className="text-warning hover:font-bold"
+                >
+                  Masuk disini
+                </Link>
+              </p>
+              <p className="text-center">
+                Login sebagai Pekerja?{" "}
+                <Link
+                  to="/login-employee"
+                  className="text-warning hover:font-bold"
+                >
+                  Masuk disini
+                </Link>
+              </p>
+            </Form>;
+          }}
+        </Formik>
       </div>
     </div>
   );
